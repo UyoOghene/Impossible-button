0c6c93c13621994b4a93725bc611139edaa7b6d4
Uyooghene <uyooghene01@gmail.com>
2024-04-02 12:58:54 +0100

    April02

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const butt = document.querySelector('#click');
+const cont = document.querySelector('.container');
+const text = document.querySelector('#text');
+const topic = document.querySelector('#topic');
 
 
 function move(event){
@@ -7,7 +10,15 @@ function move(event){
     const w =Math.floor(Math.random() * window.innerWidth);
     butt.style.top = `${w}px`;
     butt.style.left = `${h}px`;
-        console.log(butt);
+    console.log(butt);
 }
 
 butt.addEventListener('mouseover',move);
+butt.addEventListener('click',ifClicked);
+
+
+function ifClicked(){
+    cont.style.backgroundColor = 'green';
+    text.innerHTML = 'You won';
+    topic.innerHTML = '';
+}
